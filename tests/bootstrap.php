2263eb5c8e96d01aadb4363441aa606c2ec6a155
yuta.sumiyoshi <sumiyoshi102@gmail.com>
2017-03-17 11:14:06 +0900

    update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,16 +3,16 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 
-require_once 'TestClass/Domain/Models/User.php';
-require_once 'TestClass/Domain/Models/UserJob.php';
-require_once 'TestClass/Domain/Models/Item.php';
-require_once 'TestClass/Domain/Models/Order.php';
+require_once __DIR__. '/TestClass/Domain/Models/User.php';
+require_once __DIR__. 'TestClass/Domain/Models/UserJob.php';
+require_once __DIR__. 'TestClass/Domain/Models/Item.php';
+require_once __DIR__. 'TestClass/Domain/Models/Order.php';
 
-require_once 'TestClass/App/ServiceInterface.php';
-require_once 'TestClass/Domain/RepositoryInterface.php';
-require_once 'TestClass/Domain/Service.php';
-require_once 'TestClass/Infrastructure/Repository.php';
+require_once __DIR__. 'TestClass/App/ServiceInterface.php';
+require_once __DIR__. 'TestClass/Domain/RepositoryInterface.php';
+require_once __DIR__. 'TestClass/Domain/Service.php';
+require_once __DIR__. 'TestClass/Infrastructure/Repository.php';
 
-require_once 'TestClass/App/Controller.php';
-require_once 'TestClass/App/Singleton.php';
+require_once __DIR__. 'TestClass/App/Controller.php';
+require_once __DIR__. 'TestClass/App/Singleton.php';
 
